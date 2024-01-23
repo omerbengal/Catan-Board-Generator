0ccc0600b125d9a3fdaa451dfd65b27e959cbb50
Sinay Dori <dori.sinay@post.runi.ac.il>
2024-01-24 01:50:56 +0200

    created dices widget

--- lib/screens/diceScreen.dart
+++ lib/screens/diceScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class DiceScreen extends StatefulWidget {
   const DiceScreen({Key? key}) : super(key: key);
@@ -11,12 +12,57 @@ class _DiceScreenState extends State<DiceScreen> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        "Dice Screen",
-        style: TextStyle(
-          fontSize: 30,
-          fontWeight: FontWeight.bold,
-        ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 30,
+          ),
+          Dice(),
+        ],
+      ),
+    );
+  }
+}
+
+class Dice extends StatefulWidget {
+  @override
+  _DiceState createState() => _DiceState();
+}
+
+class _DiceState extends State<Dice> {
+  int firstDiceNumber = 1;
+  int secondDiceNumber = 1;
+
+  void update() {
+    setState(() {
+      firstDiceNumber = Random().nextInt(6) + 1;
+      secondDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () async {
+        for (int i = 0; i < 7; i++) {
+          await Future.delayed(Duration(milliseconds: 100), () {
+            update();
+          });
+        }
+      },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset(
+            'assets/new/dices/red$firstDiceNumber.png',
+            width: 150,
+          ),
+          Image.asset(
+            'assets/new/dices/yellow$secondDiceNumber.png',
+            width: 150,
+          ),
+        ],
       ),
     );
   }
