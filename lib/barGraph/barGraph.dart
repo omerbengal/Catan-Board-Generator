33f6dec6989aab01824d1e6b8b59e9ce87af9ec0
Sinay Dori <dori.sinay@post.runi.ac.il>
2024-02-02 12:02:07 +0200

    Stayled the bar graph - not final

--- lib/barGraph/barGraph.dart
+++ lib/barGraph/barGraph.dart
@@ -1,12 +1,63 @@
-//! problem with the import of fl_chart
-//import 'package:fl_chart/fl_chart.dart';
+import 'package:catan_board_generator/barGraph/barData.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 class MyBarGraph extends StatelessWidget {
-  const MyBarGraph({super.key});
+  final List numberCounter;
+  const MyBarGraph({Key? key, required this.numberCounter}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    BarData myBarData = BarData(
+      oneAmount: numberCounter[0],
+      twoAmount: numberCounter[1],
+      threeAmount: numberCounter[2],
+      fourAmount: numberCounter[3],
+      fiveAmount: numberCounter[4],
+      sixAmount: numberCounter[5],
+      sevenAmount: numberCounter[6],
+      eightAmount: numberCounter[7],
+      nineAmount: numberCounter[8],
+      tenAmount: numberCounter[9],
+      elevenAmount: numberCounter[10],
+      twelveAmount: numberCounter[11],
+    );
+    myBarData.intializeBarData();
+
+    return SizedBox(
+      height: 200,
+      width: 300,
+      child: BarChart(
+        BarChartData(
+          maxY: 30,
+          minY: 0,
+          gridData: FlGridData(show: false),
+          borderData: FlBorderData(show: false),
+          titlesData: FlTitlesData(
+            show: true,
+            topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+            rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+            leftTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+          ),
+          barGroups: myBarData.barData
+              .map((data) => BarChartGroupData(
+                    x: data.x,
+                    barRods: [
+                      BarChartRodData(
+                          toY: data.y.toDouble(),
+                          color: Colors.grey[800],
+                          width: 15,
+                          borderRadius: BorderRadius.circular(4),
+                          backDrawRodData: BackgroundBarChartRodData(
+                            show: true,
+                            toY: 30,
+                            color: Colors.grey[200],
+                          )),
+                    ],
+                  ))
+              .toList(),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/diceScreen.dart
+++ lib/screens/diceScreen.dart
@@ -1,3 +1,4 @@
+import 'package:catan_board_generator/barGraph/barGraph.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
 
@@ -9,6 +10,20 @@ class DiceScreen extends StatefulWidget {
 }
 
 class _DiceScreenState extends State<DiceScreen> {
+  List<int> numberCounter = [
+    4,
+    10,
+    3,
+    8,
+    4,
+    7,
+    16,
+    12,
+    2,
+    1,
+    9,
+    0,
+  ];
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -19,6 +34,12 @@ class _DiceScreenState extends State<DiceScreen> {
             height: 30,
           ),
           Dice(),
+          SizedBox(
+            height: 30,
+          ),
+          MyBarGraph(
+            numberCounter: numberCounter,
+          ),
         ],
       ),
     );
