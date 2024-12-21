78c5df8535f0111754c9c1488270d6bf7d357152
omerbengal <omerbengal7@gmail.com>
2024-12-21 17:46:07 +0200

    removed unused import

--- lib/widgets/regularBoardAndButtons.dart
+++ lib/widgets/regularBoardAndButtons.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:flutter/services.dart';
-import 'dart:io';
 import 'dart:math' as math;
 import 'package:animated_button/animated_button.dart';
 
