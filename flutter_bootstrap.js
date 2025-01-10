d1985af0228db36acfa55aa7332253a324936ef8
omerbengal <omerbengal@users.noreply.github.com>
2025-01-10 16:02:53 +0000

    deploy: 2f59d65dbd6fd8fd7222dadc43e1f107a3618d5c

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1035092789"
+    serviceWorkerVersion: "4004722841"
   }
 });
