700689385ccb28bb563b3097046576cd73f1218c
omerbengal <omerbengal@users.noreply.github.com>
2025-08-30 10:15:55 +0000

    deploy: d3f8e96dcd47a635ada7d919c3c1e295f818c82a

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "181080056e95cf498b78e43e43dfd60f",
+const RESOURCES = {"flutter_bootstrap.js": "0cd915ca577a89ac4222493e54aeaec8",
 "version.json": "9ec7522e798ed7c71ae759f901cc86c5",
 "index.html": "55d54126811044d371baa3436c03cf10",
 "/": "55d54126811044d371baa3436c03cf10",
-"main.dart.js": "da90890c99e19ccc2c59c000ad65ed7e",
+"main.dart.js": "f20f18cfc5fd307add49ffe88a4adde9",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
